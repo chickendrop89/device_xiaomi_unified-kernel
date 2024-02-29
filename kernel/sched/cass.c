2e6982501ff5b0fa2092cff0e4309a608d4d9e6c
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-28 21:07:44 -0800

    sched/cass: Clean up local variable scope in cass_best_cpu()
    Move `curr` and `idle_state` to within the loop's scope for better
    readability. Also, leave a comment about `curr->cpu` to make it clear that
    `curr->cpu` must be initialized within the loop in order for `best->cpu` to
    be valid.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -118,7 +118,6 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 {
 	/* Initialize @best such that @best always has a valid CPU at the end */
 	struct cass_cpu_cand cands[2], *best = cands;
-	unsigned long p_util, uc_min;
 	bool has_idle = false;
 	int cidx = 0, cpu;
 
@@ -134,20 +133,16 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 	 * an RCU read lock, an RCU read lock isn't needed because we're not
 	 * preemptible and RCU-sched is unified with normal RCU. Therefore,
 	 * non-preemptible contexts are implicitly RCU-safe.
+	 *
+	 * Note: @curr->cpu must be initialized before this loop ends. This is
+	 * necessary to ensure @best->cpu contains a valid CPU upon returning;
+	 * otherwise, if only one CPU is allowed and it is skipped before
+	 * @curr->cpu is set, then @best->cpu will be garbage.
 	 */
 	for_each_cpu_and(cpu, p->cpus_ptr, cpu_active_mask) {
 		/* Use the free candidate slot for @curr */
 		struct cass_cpu_cand *curr = &cands[cidx];
 		struct cpuidle_state *idle_state;
-		struct rq *rq = cpu_rq(cpu);
-
-		/* Get the capacity of this CPU adjusted for thermal pressure */
-		curr->cap_max = arch_scale_cpu_capacity(cpu) -
-				thermal_load_avg(rq);
-
-		/* Prefer the CPU that meets the uclamp minimum requirement */
-		if (curr->cap_max < uc_min && best->cap_max >= uc_min)
-			continue;
 
 		/*
 		 * Check if this CPU is idle or only has SCHED_IDLE tasks. For
@@ -206,6 +201,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync, bool rt
 		 * If @best == @curr then there's no need to compare them, but
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
+		curr->cpu = cpu;
 		if (best == curr ||
 		    cass_cpu_better(curr, best, prev_cpu, sync)) {
 			best = curr;
